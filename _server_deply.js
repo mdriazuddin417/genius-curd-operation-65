f0cc1b11b3b4484aaee78772edf14c9daf758fcb
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-04-26 10:11:59 +0600

    addeing steps

---
+++ _server_deply.js
@@ -0,0 +1,12 @@
+/* 
+ Ont time for your computer
+ 1. create heroku account
+ 2. verify email
+ 3. install heroku cli
+ 4. heroku login
+ -------------------------
+
+ ** For eac project one time
+ 
+ 1.  
+*/
