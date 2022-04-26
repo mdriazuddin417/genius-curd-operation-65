ab0896831f446b9cc965150b8cd055fb00993beb
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-04-26 11:01:59 +0600

    steps add

--- _server_deply.js
+++ _server_deply.js
@@ -11,4 +11,17 @@
  1.  heroku create
  2. make sure you:git add . git commit. git push
  3.git push heroku main
-*/
+ 4. Go to heroku Dashboard > Current Project > Settings > Reveal Config >
+ 5. copy past config vars from your .env file
+ 6. Make sure you have white listed all ip address to access mongodb
+ -----------------------------------------
+ ***Update SERVER
+ 1.. Make Change
+ 2. make sure you:git add . git commit. git push
+ 3.git push heroku main 
+ =================================
+ **Connect Server with Client
+ 1. replace localhost by heroku  link
+ 2. npm run build
+ 3. firebase deploy
+ */
